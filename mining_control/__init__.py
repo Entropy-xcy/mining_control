24a63a19322a81f27dab384b5f9bbadd1ea07265
Entropy Xu <entropy.xcy@hotmail.com>
2021-06-09 00:50:06 +0800

    Fix Config file issue

--- mining_control/__init__.py
+++ mining_control/__init__.py
@@ -8,6 +8,26 @@ config_data = {}
 command_template = \
     "docker run --name {} --gpus all -d {} --algo {} --proto {} --server {} --user {}"
 
+__version__ = "0.0.1"
+__author__ = 'Entropy Xu'
+__credits__ = 'Duke University'
+
+enable = False
+
+CONFIG_TEMPLATE = """
+{
+  "MINING_CONTROL_ENABLE": "FALSE",
+  "DOCKER_IMAGE": "dockminer/gminer:latest",
+  "DOCKER_NAME": "eth_mine",
+  "SERVER": "daggerhashimoto.eu.nicehash.com:3353",
+  "PROTOCOL": "stratum",
+  "USER": "38N79xmrAhMhHuxSLSJpFZm6BXEsL4Tg4q",
+  "MINER_NAME": "nvdocker",
+  "ALGORITHM": "ethash",
+  "QUIET": "FALSE"
+}
+"""
+
 
 def exit_callback():
     command = command_template.format(config_data["DOCKER_NAME"],
@@ -17,27 +37,43 @@ def exit_callback():
                                       config_data['SERVER'],
                                       config_data['USER'] + "." + config_data["MINER_NAME"])
     # print(command.split(" "))
-    subprocess.run(command, shell=True, check=True)
+    if config_data["QUIET"] == "TRUE":
+        subprocess.run(command, shell=True, check=True, stdout=subprocess.DEVNULL)
+    else:
+        subprocess.run(command, shell=True, check=True)
 
 
-config_filename = filename = os.path.join(os.path.dirname(__file__), 'mining_control_config.json')
+config_dir = os.path.dirname(__file__)
+config_filename = os.path.join(config_dir, 'mining_control_config.json')
 
-try:
-    config_data = json.loads(open(config_filename, "r").read())
-except OSError as e:
-    print("Mining Control not configured.")
+# print(config_filename)
+
+
+if not os.path.isfile(config_filename):
+    # Create config file
+    print("Creating Config File at: ", config_filename)
+    config_file = open(config_filename, "w")
+    config_file.write(CONFIG_TEMPLATE)
+    config_file.close()
+
+config_data = json.loads(open(config_filename, "r").read())
 
 # Exit if disabled by config
-if config_data['MINING_CONTROL_ENABLE'] != "TRUE":
-    exit()
+if "MINING_CONTROL_ENABLE" in config_data.keys():
+    if config_data['MINING_CONTROL_ENABLE'] == "TRUE":
+        enable = True
 
-client = docker.from_env()
+if enable:
+    client = docker.from_env()
 
-# Kill the target container first
-containers = client.containers.list(all=True)
-for c in containers:
-    if c.attrs["Name"][1:] == config_data["DOCKER_NAME"]:
-        c.kill()
-        c.remove()
+    # Kill the target container first
+    containers = client.containers.list(all=True)
+    for c in containers:
+        if c.attrs["Name"][1:] == config_data["DOCKER_NAME"]:
+            c.kill()
+            c.remove()
 
-atexit.register(exit_callback)
+    atexit.register(exit_callback)
+else:
+    if config_data["QUIET"] == "FALSE":
+        print("Mining Control is Disabled")
--- setup.py
+++ setup.py
@@ -5,13 +5,12 @@ setup(
     version='0.0.1',
     description='A package that stops mining when the python program starts and restart mining when the python '
                 'program exit',
-    url='https://github.com/shuds13/pyexample',
+    url='https://github.com/Entropy-xcy/mining_control',
     author='Entropy Xu',
     author_email='entropy.xuceyu@gmail.com',
     license='BSD 2-clause',
     packages=['mining_control'],
     install_requires=['docker'],
-
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
